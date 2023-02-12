2a1d189cc791f5f86d2450cf058ebbe15854c2c1
McThuggets <dylan.reuben1509@gmail.com>
2023-02-12 15:48:18 +0200

    get individual pokemon info

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -2,13 +2,6 @@ import React, { useState } from 'react'
 import PokemonCard from './PokemonCard'
 
 export default function Pokedex() {
-
-    //make request to https://pokeapi.co/api/v2/pokemon/1/
-    //iterate over list and display:
-    //                                  -name
-    //                                  -button to URL
-    // use useState to update PokemonCard when a URL is clicked
-    // display 'name' 'picture' 'ability 1' 'ability 2'
     
   const [pokemonListUI, setpokemonListUI] = useState(<h2 className='pokemon-card'> Pokemon will appear here...</h2>)
 
@@ -17,18 +10,15 @@ export default function Pokedex() {
   const pokedexRequest = () => { 
 
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json())
-    .then(allPokemon => { allPokemon.results.forEach(pokemon => { pokemonList.push(pokemon) })  } )
-    .then( () => {
-                    setpokemonListUI( pokemonList.map( (pokemon, index) => {
-                      return( <PokemonCard pokemon={pokemon} index={index} /> )
-                      })
-                    )
-                } )
+      .then(response => response.json())
+      .then(allPokemon => { allPokemon.results.forEach(pokemon => { pokemonList.push(pokemon) })  } )
+      .then( () => {
+                      setpokemonListUI( pokemonList.map( (pokemon, index) => {
+                        return( <PokemonCard pokemon={pokemon} key={index} /> )
+                        })
+                      )
+                  } )
 
-    console.log(pokemonList)
-
-    
   }
 
 
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -3,12 +3,22 @@ import React, {useState} from 'react'
 
 export default function PokemonCard( pokemon, index ) {
 
-  
+  const [pokeInfo, setpokeInfo] = useState('')
+
+
+  const loadPokeInfo = () => {
+
+    fetch( pokemon.pokemon.url )
+    .then(response => response.json() )
+    .then( pokeInfo => console.log(pokeInfo) )
+
+  }
 
   return (
 
     <>
-        <h2 className='pokemon-card' > {pokemon.pokemon.name} </h2>
+        <h2 className='pokemon-card' onClick={loadPokeInfo}> {pokemon.pokemon.name} </h2>
+        { pokeInfo }
     </>
   )
 }
