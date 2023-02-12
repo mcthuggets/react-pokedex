e9c1510dc62eec35458dc3843f5cec1003497df8
McThuggets <dylan.reuben1509@gmail.com>
2023-02-12 12:54:33 +0200

    first commit -navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+
+    <div className="navbar">
+
+        <img src='images/pokeball.png'></img>
+        <img src='images/pokemon-title.png'></img>
+    </div>
+    
+    <div className='backgroundColor'>
+
+    </div>
+
+    </>
+  )
+}
+
+
+
