575c24ec19b863d6e70f53d7278cc19b213c7a51
McThuggets <dylan.reuben1509@gmail.com>
2023-02-12 20:11:25 +0200

    reveal pokemon image onclick

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -5,20 +5,29 @@ export default function PokemonCard( pokemon, index ) {
 
   const [pokeInfo, setpokeInfo] = useState('')
 
+  const [imgURL, setimgURL] = useState('/images/question-mark.png')
+
 
   const loadPokeInfo = () => {
 
     fetch( pokemon.pokemon.url )
     .then(response => response.json() )
-    .then( pokeInfo => console.log(pokeInfo) )
+    .then( pokeInfo => setimgURL(pokeInfo.sprites.front_default) )
 
   }
 
   return (
 
     <>
-        <h2 className='pokemon-card' onClick={loadPokeInfo}> {pokemon.pokemon.name} </h2>
-        { pokeInfo }
+
+      <div className='pokemon-card' onClick={loadPokeInfo}>
+
+        <h2 className='pokemon-card' > {pokemon.pokemon.name} </h2>
+
+        <img src={imgURL} width={'100px'} height={'100px'}></img>
+
+      </div>
+        
     </>
   )
 }
