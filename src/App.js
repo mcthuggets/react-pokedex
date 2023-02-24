6ae2a919be64685ee2ca9842d527858801f82bd5
McThuggets <dylan.reuben1509@gmail.com>
2023-02-24 16:02:03 +0200

    a change that was left behind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import Banner from './Components/Banner';
-import Navbar from './Components/Navbar';
-import Pokedex from './Components/Pokedex';
-import PokemonCard from './Components/PokemonCard';
 import Home from './Pages/Home';
+//
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Pokemon from './Pages/Pokemon';
 
 function App() {
 
   return (
   <>
-    <Home />
+    <Router>
+      <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/Pokemon' element={<Pokemon/>} />
+      </Routes>
+    </Router>
   </>
   );
 }
